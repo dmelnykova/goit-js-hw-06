1dc849a18eb6d4d274af7618a4ddda4b6c1e8cad
dariameln1 <dariamelnykova1@gmail.com>
2023-09-14 21:13:09 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const ingredientItems = document.createElement("li");
+
+const liArray = ingredients.map(ingredient => {
+  const ingredientItems = document.createElement("li");
+  ingredientItems.classList.add('item');
+  ingredientItems.textContent = `${ingredient}`;
+  return ingredientItems;
+});
+
+list.append(...liArray);
+console.log(list);
+
