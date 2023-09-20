bcada165cfe8b74e2716cf527a19861d3661bbfb
dariameln1 <dariamelnykova1@gmail.com>
2023-09-20 19:25:22 +0200

    task-05

---
+++ src/js/task-05.js
@@ -0,0 +1,9 @@
+const nameInput = document.querySelector('#name-input');
+
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', event => {
+    const inputValue = event.target.value;
+    nameOutput.textContent = inputValue || 'Anonymous';
+});
+
