e8e887e00aaaadca8094567b7f38b20ce138fc64
dariameln1 <dariamelnykova1@gmail.com>
2023-09-21 18:59:01 +0200

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === '' || password === '') {
+    alert('Please fill in all the fields!');
+    return;
+  }
+
+  const formData = {
+    email,
+    password
+  };
+
+  console.log('Form data:', formData);
+  form.reset();
+}
